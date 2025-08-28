6597dc0818c82ae2e21f39f26171a9f316ea82fd
Shammi-Akther <shammisumi137@gmail.com.>
2025-08-28 10:56:22 +0600

    update ui design more

--- src/components/Providers.jsx
+++ src/components/Providers.jsx
@@ -1,11 +1,6 @@
-'use client'
-import { GoogleOAuthProvider } from '@react-oauth/google';
+'use client';
 import { SessionProvider } from 'next-auth/react';
 
 export default function Providers({ children }) {
-  return (
-    <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}>
-      <SessionProvider>{children}</SessionProvider>
-    </GoogleOAuthProvider>
-  );
+  return <SessionProvider>{children}</SessionProvider>;
 }
